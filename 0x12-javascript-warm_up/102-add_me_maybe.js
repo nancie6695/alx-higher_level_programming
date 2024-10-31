8d4b58ee846da9e4ed3059d2a7f5da27ad8159e5
nancie6695 <nanciek004@gmail.com>
2024-10-31 13:58:51 +0300

    Basics Of JS from Datatypes TILL functions ...also alot of arguments (process.argv)

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node 
+exports.addMe = function(number, theFunction){
+	theFunction(++number);
+}
+
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node 
+
+for (const multi of ['C is fun', 'python is cool', 'Javascript is amazing']){
+	console.log(multi);
+}
+
+
+
